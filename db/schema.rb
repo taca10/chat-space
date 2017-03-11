8df7454508b715f5ba2a0b15dd482df06640379f
takato <taka.ur.0505@gmail.com>
2017-03-11 23:39:28 +0900

    haml,migration 編集

--- db/schema.rb
+++ db/schema.rb
@@ -24,12 +24,13 @@ ActiveRecord::Schema.define(version: 20170311081828) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip",     limit: 255
     t.string   "last_sign_in_ip",        limit: 255
-    t.string   "name",                   limit: 255
+    t.string   "name",                   limit: 255,              null: false
     t.datetime "created_at",                                      null: false
     t.datetime "updated_at",                                      null: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["name"], name: "index_users_on_name", using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
