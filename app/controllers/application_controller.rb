126d8b814c378683372946995937206295171d97
takato <taka.ur.0505@gmail.com>
2017-03-18 23:42:33 +0900

    haml scss 作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,12 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
+  
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+  def after_sign_in_path_for(resource)
+    root_path
   end
 end
