63816a628f0b37bcf0e77355c42f85ac8e86f969
takato <taka.ur.0505@gmail.com>
2017-03-29 19:59:10 +0900

    edit group_view

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,4 +1,19 @@
 class GroupsController < ApplicationController
-  def new
+  def index
+
   end
+
+  def new
+    @group = Group.new
+  end
+
+  def create
+    Group.create(group_params)
+    redirect_to  controller: :messages, action: :index
+  end
+
+   def group_params
+      params.require(:group).permit(:name )
+   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root  "messages#index"
-  resources :groups, only: [:new]
+  resources :groups, only: [:new, :create, :edit]
 end
