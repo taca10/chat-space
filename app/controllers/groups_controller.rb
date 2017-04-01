4690cfd485daae79e646a5a59c73485f1759c076
takato <taka.ur.0505@gmail.com>
2017-04-02 02:19:02 +0900

    create edit function

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -11,15 +11,25 @@ class GroupsController < ApplicationController
   def create
     @group = Group.new(group_params)
     if @group.save
-      redirect_to root_path, notice: 'ログインに成功しました'
+      redirect_to root_path, notice: 'グループ作成に成功しました'
     else
-      flash.now[:alert] = "グループは作成されませんでした"
+      flash.now[:alert] = 'グループは作成されませんでした'
       render :new
     end
   end
 
   def edit
+    @group = Group.find(params[:id])
+  end
 
+  def update
+    @group = Group.find(params[:id])
+    if @group.update(group_params)
+       redirect_to root_path, notice: "グループ編集に成功しました"
+    else
+      flash.now[:alert] = 'グループは編集されませんでした'
+      render :new
+    end
   end
 
   def group_params
