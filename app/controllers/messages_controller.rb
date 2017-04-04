ef3b1b5afb29ea5ff108bd97549b855119c20afb
takato <taka.ur.0505@gmail.com>
2017-04-04 11:42:15 +0900

    crate_message_function

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,7 +1,29 @@
 class MessagesController < ApplicationController
+
+  before_action :get_group, only: [:index, :create]
+
   def index
     @groups = current_user.groups.order('created_at DESC')
-    @group = Group.find(params[:group_id])
     @users = @group.users
+    @message = Message.new
+    @messages = @group.messages
+  end
+
+  def new
+  end
+
+  def create
+  Message.create(message_params)
+  redirect_to  group_messages_path(@group)
+  end
+
+  private
+  def message_params
+    params.require(:message).permit(:text).merge(group_id: params[:group_id], user_id: current_user.id)
+  end
+
+  def get_group
+  @group = Group.find(params[:group_id])
   end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root  "groups#index"
   resources :groups, only: [:new, :create, :edit, :update] do
-  resources :messages, only: [:index]
+  resources :messages, only: [:index, :new, :create]
   end
 end
