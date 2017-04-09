6ce76c9bf42602773ff075817e28331bcf4ed4e2
takato <taka.ur.0505@gmail.com>
2017-04-09 15:44:18 +0900

    add_test_message

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -6,5 +6,10 @@ describe Message do
      message.valid?
      expect(message.errors[:text]).to include("を入力してください。")
     end
+
+    it "is valid with a text" do
+      message  = build(:message)
+      expect(message).to be_valid
+    end
   end
 end
