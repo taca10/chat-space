b52b2f7bfc807a70772620dfe2952d5b00d2119d
takato <taka.ur.0505@gmail.com>
2017-04-09 20:32:39 +0900

    edit_factories_messages

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -1,9 +1,9 @@
 FactoryGirl.define do
 
   factory :message do
-    text           {Faker::Lorem.word}
-    image          {Faker::Avatar.image}
-    group_id       {Faker::Number.number(1)}
-    user_id        {Faker::Number.number(2)}
+    text           { Faker::Lorem.word }
+    image          { Faker::Avatar.image }
+    group_id       "1"
+    user_id        "2"
   end
 end
