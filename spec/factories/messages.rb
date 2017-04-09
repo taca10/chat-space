cb6ab2b7ed32ea6d45f51ed9f83e0f78412137f7
takato <taka.ur.0505@gmail.com>
2017-04-09 15:26:48 +0900

    add_factorygirl

---
+++ spec/factories/messages.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+
+  factory :message do
+    text           "こんにちは"
+  end
+end
--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 describe Message do
   describe '#create' do
     it "is invalid without a text" do
-     message = Message.new(text: "")
+     message = build(:message, text: "")
      message.valid?
      expect(message.errors[:text]).to include("を入力してください。")
     end
