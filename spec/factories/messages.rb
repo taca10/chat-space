2eea6f4bf8e2d264e78971f2da96175fc4be6121
takato <taka.ur.0505@gmail.com>
2017-04-09 16:15:52 +0900

    add_faker_date

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -1,6 +1,9 @@
 FactoryGirl.define do
 
   factory :message do
-    text           "こんにちは"
+    text           {Faker::Lorem.word}
+    image          {Faker::Avatar.image}
+    group_id       {Faker::Number.digit}
+    user_id        {Faker::Number.digit}
   end
 end
