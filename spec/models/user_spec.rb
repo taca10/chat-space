96fda0f6b6e645561033535ac7efb1054f25f467
takato <taka.ur.0505@gmail.com>
2017-04-10 16:15:29 +0900

    add_invalid_with_a_password_that_has_less_than_7_characters

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -26,15 +26,20 @@ describe User do
       expect(user.errors[:password_confirmation]).to include("とパスワードの入力が一致しません。")
     end
     it "is invalid with a duplicate email address" do
-      user = create(:user, id: 1)
+      user = create(:user)
       another_user = build_stubbed(:user, email: user.email)
       another_user.valid?
       expect(another_user.errors[:email]).to include("はすでに存在します。")
     end
     it "is invalid with a name that has more than 7 characters " do
-      user = build(:user, name: "aaaaaaaa")
+      user = build(:user, name: "aaaaaaa")
       user.valid?
       expect(user.errors[:name]).to include("は6文字以内で入力してください。")
+    end
+    it "is invalid with a password that has less than 7 characters " do
+      user = build(:user, password: "00000", password_confirmation: "00000")
+      user.valid?
+      expect(user.errors[:password][0]).to include("は6文字以上で入力してください。")
     end
   end
 end
