a455dae25cbb126ec659fe1e76a731fbe9b6aa4c
takato <taka.ur.0505@gmail.com>
2017-04-10 12:20:12 +0900

    add_valid_all_value

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 describe User do
   describe '#create' do
+    it "is valid with a id, nickname, email, password, password_confirmation" do
+      user = build_stubbed(:user)
+      expect(user).to be_valid
+    end
     it "is invalid without a name" do
      user =  build(:user, name: "")
      user.valid?
