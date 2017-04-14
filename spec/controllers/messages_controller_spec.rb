d6af4ca0e5cdd19f9f1a72b06d32ea0294423560
takato <taka.ur.0505@gmail.com>
2017-04-14 22:20:27 +0900

    edit_messages_controller_spec.rb

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 
 describe MessagesController, type: :controller do
 
-  let(:group){create(:group)}
-  let(:user){create(:user)}
-  let(:message){build(:message)}
+  let(:group) { create(:group) }
+  let(:user) { create(:user) }
+  let(:message) { build(:message) }
 
   before do
     sign_in user
@@ -20,19 +20,16 @@ describe MessagesController, type: :controller do
       expect(assigns(:group)).to eq group
     end
     it "assigns the requested contact to @groups" do
-      @groups = user.groups.first
       get :index, id: message, group_id: group.id
       groups = user.groups
       expect(assigns(:groups)).to eq groups
     end
     it "assigns the requested contact to @users" do
-      @users = group.users.first
       get :index, id: message, group_id: group.id
       users = group.users
       expect(assigns(:users)).to eq users
     end
     it "assigns the requested contact to @messages" do
-      @messages = group.messages.first
       get :index, id: message, group_id: group.id
       messages = group.messages
       expect(assigns(:messages)).to eq messages
