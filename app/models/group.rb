44906b236a02b2bfafa74a64eb3494ea32557922
takato <taka.ur.0505@gmail.com>
2017-04-27 16:53:29 +0900

    edit_model_group.rb

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,7 +2,7 @@ class Group < ApplicationRecord
   has_many :messages
   has_many :group_users
   has_many :users, through: :group_users
-  accepts_nested_attributes_for :users
+  accepts_nested_attributes_for :group_users
 
   validates :name, presence: true
 
