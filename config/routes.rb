b19ee72bfc1a0f825d0a7a23060ce6c7bd8b88b0
takato <taka.ur.0505@gmail.com>
2017-05-08 13:14:25 +0900

    edit_routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,5 @@ Rails.application.routes.draw do
   resources :groups, only: [:new, :create, :edit, :update] do
     resources :messages, only: [:index, :create]
   end
-  resources :users, only: [] do
-    collection do
-      get  :search
-    end
-  end
+  get   'users/search'      => 'users#search'
 end
